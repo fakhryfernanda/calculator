88a62992a514d072924b81ed0b72619586e258ac
fakhryfernanda <mff023@gmail.com>
2022-05-08 18:38:04 +0700

    Create function for operate

--- script.js
+++ script.js
@@ -24,3 +24,27 @@ function clearMonitor() {
     resultVal = 0;
     showResult(0);
 };
+
+function add(a,b) {return (a + b)};
+function subtract(a,b) {return (a - b)};
+function multiply(a,b) {return (a * b)};
+function divide(a,b) {return (a / b)};
+
+function operate(leftOperand, rightOperand, operator) {
+    switch(operator) {
+        case 'add':
+            return add(leftOperand, rightOperand);
+            break;
+        case 'substract':
+            return substract(leftOperand, rightOperand);
+            break;
+        case 'multiply':
+            return multiply(leftOperand, rightOperand);
+            break;        
+        case 'divide':
+            return divide(leftOperand, rightOperand);
+            break;    
+    
+    }
+};
+
