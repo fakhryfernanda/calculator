0ae11e174b2a8d66ea2b5badbc51447b00cb2984
fakhryfernanda <mff023@gmail.com>
2022-05-10 14:40:06 +0700

    Change calculator logic
    Now it works depends on the pointer (i.e. leftOperand, rightOperand, operator or result)

--- script.js
+++ script.js
@@ -3,68 +3,116 @@ const resultDiv = calculator.querySelector('.result');
 const numbers = calculator.querySelectorAll('[data-button="number"]');
 const operators = calculator.querySelectorAll('[data-button*="operator"]');
 const equalSign = calculator.querySelector('[data-button="equal-sign"]');
+const backSpace = calculator.querySelector('[data-button="backspace"]');
 const clearButton = calculator.querySelector('[data-button="clear"]');
 
 const myCalc = new Calculator();
 
 // VARIABLES
 
+operationSign = {
+    'add-operator' : '&plus;', 
+    'substract-operator' : '&minus;', 
+    'multiply-operator' : '&times;', 
+    'divide-operator' : '&divide;',
+    'equal-sign' : '&equals;'
+};
+
+const actions = ['leftOperand', 'operator', 'rightOperand', 'result'];
+let pointer = actions[0];
+
 let operatorClicked = false;
 let equalSignClicked = false;
 
-let resultText = '';
-let currentValue = 0;
-let variables = [];
+let currentValue = 0; // berkaitan dengan nilai asli leftOperand atau rightOperand
+let resultText = ''; // berkaitan dengan apa yang akan ditampilkan di monitor
+let variables = []; // riwayat operasi
 
 // NUMBERS
 
 numbers.forEach(number => {
     number.addEventListener('click', e => {
-    
-    if (variables.length === 5) {
-        monitorCalculation(`Ans = ${variables[4]}`);
-        variables = [];
-        resultText = '';
-        currentValue = 0;
-    };
-    currentValue = 10*currentValue + parseInt(e.target.innerText);
-    resultText += e.target.innerText;
-    monitorResult(resultText);
 
+        // jika perhitungan sebelumnya sudah selesai, tampilkan hasil perhitungan sebelumnya pada previous-calculation
+        if (variables.length === 5) {
+            clearAll();
+        };
+        
+        // jika operator sudah ditekan, pindahkan pointer
+        if (operatorClicked) {
+            pointer = actions[2];
+        };    
+
+        // proses utama
+        currentValue = 10*currentValue + parseInt(e.target.innerText);
+
+        if (pointer === actions[0]) {
+            variables[0] = currentValue
+        } else if (pointer === actions[2]) {
+            variables[2] = currentValue
+        };
+
+        monitorResult();
     });
 });
 
 // OPERATORS
 
+// memastikan operator tidak muncul dua kali
 !operatorClicked && operators.forEach(operator => {
     operator.addEventListener('click', (e) => {
+        
+        // menandai bahwa operator sudah ditekan
         operatorClicked = true;
-        if (resultText === '') {resultText += '0'};
 
-        variables.push(currentValue);
-        variables.push(e.target.getAttribute('data-button'));
-        resultText += ' ' + e.target.innerText + ' ';
+        // jika operator ditekan sebelum memasukkan leftOperand, otomatis leftOperand = 0
+        if (variables[0] === undefined) {
+            variables[0] = 0;
+        };
+
+        // memindahkan pointer
+        pointer = actions[1];
+
+        // memasukkan operator ke array variables
+        variables[1] = e.target.getAttribute('data-button');
+
         currentValue = 0;
 
-        monitorResult(resultText);
+        monitorResult();
     });
 });
 
 // EQUAL SIGN
 
+// mencegah equal-sign ditekan dua kali
 !equalSignClicked && equalSign.addEventListener('click', (e) => {
+    // menandai equal-sign sudah ditekan
     equalSignClicked = true;
 
-    variables.push(currentValue);
-    variables.push(e.target.getAttribute('data-button'));
-    resultText += ' ' + e.target.innerText + ' ';
-    // calculationText = resultText;
-    monitorCalculation(resultText);
+    // memindahkan pointer
+    pointer = actions[3];
+
+    // memasukkan equal-sign ke array variables
+    variables[3] = e.target.getAttribute('data-button');
+
+    // menampilkan riwayat operasi pada previous-result div
+    monitorCalculation();
+
+    // melakukan perhitungan berdasarkan leftOperand, operator, dan rightOperand
     currentValue = myCalc.calculate(variables[0], variables[2], variables[1]);
-    variables.push(currentValue);
-    resultText += currentValue;
-    monitorResult(currentValue);
+
+    // memasukkan hasil perhitungan ke array variables
+    variables[4] = currentValue;
+
+    // console.log(variables);
+    
+    // menampilkan hasil perhitungan pada monitor
+    monitorResult();
 });
+
+// BACKSPACING
+
+backSpace.addEventListener('click', backSpacing);
 
 // CLEAR MONITOR
 
@@ -72,24 +120,80 @@ clearButton.addEventListener('click', clearAll);
 
 // FUNCTIONS
 
-function monitorCalculation(calculationText) {
+function monitorCalculation() {
     const calculationDiv = calculator.querySelector('.previous-calculation');
-    calculationDiv.innerText = calculationText;
+
+    let monitorText;
+    if (pointer !== null) {
+        monitorText = variables[0] + ' ' + operationSign[variables[1]] + ' ' + variables[2] + ' ' + operationSign[variables[3]];
+    } else {
+        monitorText = `Ans = ${variables[4]}`;
+    };
+
+    calculationDiv.innerHTML = monitorText;
 };
 
-function monitorResult(resultText) {
+function monitorResult() {
     const resultDiv = calculator.querySelector('.result');
-    resultDiv.innerText = resultText;
+    let monitorText;
+
+    switch(pointer) {
+        case actions[0]:
+            monitorText = variables[0];
+            break;
+        case actions[1]:
+            monitorText = variables[0] + ' ' + operationSign[variables[1]];
+            break;
+        case actions[2]:
+            monitorText = variables[0] + ' ' + operationSign[variables[1]] + ' ' + variables[2];
+            break;
+        case actions[3]:
+            monitorText = variables[4];
+            break;
+        case null:
+            monitorText = 0;
+    };
+
+    resultDiv.innerHTML = monitorText;
+};
+
+function backSpacing() {
+    switch(pointer) {
+        case actions[0]:
+            currentValue = Math.floor(currentValue / 10);
+            break;
+        case actions[1]:
+            operatorClicked = false;
+            pointer = actions[0];
+            variables = variables.slice(0, -1);
+            break;
+        case actions[2]:
+            currentValue = Math.floor(currentValue / 10);
+            break;
+        case actions[3]:
+            clearAll();
+            break;
+    };
+
+    resultText = resultText.slice(0, -1);
+    monitorResult(resultText);
+
+    if (currentValue === 0) {
+        monitorResult(0);
+    };
 };
 
 function clearAll() {
+
+    pointer = null;
+    monitorCalculation();
+    monitorResult();
+    pointer = actions[0];
+    
     operatorClicked = false;
     equalSignClicked = false;
     variables = [];
-
     currentValue = 0;
-    resultText = '';
-    monitorResult(0);
 };
 
 function Calculator() {
